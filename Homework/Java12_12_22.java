d25a41955672098f214d4ef7743ac80e2452cfae
Maryna7068 <119616320+Maryna7068@users.noreply.github.com>
2022-12-27 14:03:25 +0000

    change

--- Homework/Java12_12_22.java
+++ Homework/Java12_12_22.java
@@ -1,8 +1,9 @@
 public class Java12_12_22{
-    public static void main(String[] ardest)}{ 
-        public static void main(String[] args) {
+    public static void main(String[] ardest){
+     System.out.println("Hello World");
           
         }
     }
+    
     
 
